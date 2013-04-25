27c55a916e145a83abfd9f4498ef55e72aea8139
crazyhuaer <20549304@163.com>
2013-04-25 09:52:36 +0800

    [Branch_temp][C]test the branch_temp

---
+++ tmpcode/C/study_201304/c_clssroom/c_classroom.c
@@ -0,0 +1,10 @@
+
+#include "c_classroom.h"
+
+int main(int argc,char *argv[])
+{
+	char	a = 'a';
+	//printf("hello,world!\n");
+	printf("a=%d\n",a);
+	return 0;
+}
---
+++ tmpcode/C/study_201304/c_clssroom/c_classroom.h
@@ -0,0 +1,8 @@
+
+#ifndef _C_CLASSROOM_H_
+#define _C_CLASSROOM_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#endif
