308d9e1e72d01ef35370a3e5ca3cf6c4bb2fc0aa
Geeknimo <h20549304@gmail.com>
2013-09-16 21:49:36 +0800

    [Master][Arduino]add web client for remote control fans

---
+++ Arduino/test_ethernet/webserver/webclient_get/webclient_get.ino
@@ -0,0 +1,118 @@
+/*
+   Web client sketch for IDE v1.0.1 and w5100/w5200
+   Uses POST method.
+   Posted November 2012 by SurferTim
+*/
+
+#include <SPI.h>
+#include <Ethernet.h>
+
+byte mac[] = {  
+  0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
+
+//Change to your server domain
+char serverName[] = "www.baidu.com";
+
+// change to your server's port
+int serverPort = 80;
+
+// change to the page on that server
+char pageName[] = "/";
+
+EthernetClient client;
+int totalCount = 0; 
+int loopCount = 0;
+// insure params is big enough to hold your variables
+char params[32];
+
+void setup() {
+  Serial.begin(9600);
+
+  // disable SD SPI
+  pinMode(4,OUTPUT);
+  digitalWrite(4,HIGH);
+
+  Serial.print(F("Starting ethernet..."));
+  if(!Ethernet.begin(mac)) Serial.println(F("failed"));
+  else Serial.println(Ethernet.localIP());
+
+  delay(2000);
+  Serial.println(F("Ready"));
+}
+
+void loop()
+{
+  if(loopCount < 30)
+  {
+    delay(1000);
+  }
+  else
+  {
+    loopCount = 0;
+    // params must be url encoded.
+    sprintf(params,"temp1=%i",totalCount);     
+    if(!postPage(serverName,serverPort,pageName,params)) Serial.print(F("Fail "));
+    else Serial.print(F("Pass "));
+    totalCount++;
+    Serial.println(totalCount,DEC);
+  }    
+
+  loopCount++;
+}
+
+
+byte postPage(char* domainBuffer,int thisPort,char* page,char* thisData)
+{
+  int inChar;
+  char outBuf[64];
+
+  Serial.print(F("connecting..."));
+
+  if(client.connect(domainBuffer,thisPort))
+  {
+    Serial.println(F("connected"));
+
+    // send the header
+    sprintf(outBuf,"POST %s HTTP/1.1",page);
+    client.println(outBuf);
+    sprintf(outBuf,"Host: %s",domainBuffer);
+    client.println(outBuf);
+    client.println(F("Connection: close\r\nContent-Type: application/x-www-form-urlencoded"));
+    sprintf(outBuf,"Content-Length: %u\r\n",strlen(thisData));
+    client.println(outBuf);
+
+    // send the body (variables)
+    client.print(thisData);
+  } 
+  else
+  {
+    Serial.println(F("failed"));
+    return 0;
+  }
+
+  int connectLoop = 0;
+
+  while(client.connected())
+  {
+    while(client.available())
+    {
+      inChar = client.read();
+      Serial.write(inChar);
+      connectLoop = 0;
+    }
+
+    delay(1);
+    connectLoop++;
+    if(connectLoop > 10000)
+    {
+      Serial.println();
+      Serial.println(F("Timeout"));
+      client.stop();
+    }
+  }
+
+  Serial.println();
+  Serial.println(F("disconnecting."));
+  client.stop();
+  return 1;
+}
---
+++ Arduino/test_ethernet/webserver/webclient_post/webclient_remotecontrol/webclient_remotecontrol.ino
@@ -0,0 +1,180 @@
+/*
+   Web client sketch for IDE v1.0.1 and w5100/w5200
+   Uses GET method.
+   Posted October 2012 by SurferTim
+   Last modified August 23, 2013
+*/
+
+#include <SPI.h>
+#include <Ethernet.h>
+
+// this must be unique
+byte mac[] = {  0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
+
+// change to your network settings
+IPAddress ip(192,168,0,147);
+IPAddress gateway(192, 168, 0, 1);
+
+//IPAddress ip(192,168,1,215);
+//IPAddress gateway(192,168,1,1);
+
+IPAddress subnet(255, 255, 255, 0);
+
+// change to your server
+//IPAddress server(74,125,227,16); // Google
+//IPAddress server(61,135,169,105); // Baidu
+IPAddress server(50,31,152,47);
+//IPAddress server(192,168,1,112);
+//IPAddress server(192,168,1,103); // Google
+//Change to your domain name for virtual servers
+char serverName[] = "www.geeknimo.com";
+// If no domain name, use the ip address above
+//char serverName[] = "192.168.1.112";
+
+// change to your server's port
+int serverPort = 80;
+
+EthernetClient client;
+int remote_data=0;
+int loopCount = 0;
+char pageAdd[64];
+
+void setup() {
+  Serial.begin(9600);
+
+  // disable SD SPI
+  pinMode(4,OUTPUT);
+  digitalWrite(4,HIGH);
+  
+  pinMode(8,OUTPUT);
+  digitalWrite(8,LOW);
+  
+  pinMode(9,OUTPUT);
+  digitalWrite(9,LOW);
+  
+  // Start ethernet
+  Serial.println(F("Starting ethernet..."));
+  Ethernet.begin(mac, ip, gateway, gateway, subnet);
+
+  // If using dhcp, comment out the line above 
+  // and uncomment the next 2 lines
+
+  if(!Ethernet.begin(mac)) Serial.println(F("failed"));
+  else Serial.println(F("ok"));
+  digitalWrite(10,HIGH);
+
+  Serial.println(Ethernet.localIP());
+
+  delay(2000);
+  //Serial.println(F("Ready"));
+}
+
+void loop()
+{
+  if(loopCount < 10)
+  {
+    // if loopCount is less than 30, just delay a second
+    delay(1000);
+  }
+  else
+  {
+    // every thirty seconds this runs
+    loopCount = 0;
+
+    // Modify next line to load different page
+    // or pass values to server
+    //sprintf(pageAdd,"/",totalCount);
+
+    //sprintf(pageAdd,"/arduino.php?test=%u",totalCount);
+    //sprintf(pageAdd,"/question2answer/index.php",totalCount);
+    //sprintf(pageAdd,"/remotecontrol/index.php?data=%u",remote_data);
+    sprintf(pageAdd,"/remotecontrol/index.php?flag=%u",remote_data);
+    
+    //Serial.print(pageAdd);
+    if(!getPage(server,serverPort,pageAdd)) Serial.print(F("Fail "));
+    else  Serial.print(F("Pass "));
+   
+  }    
+
+  loopCount++;
+}
+
+byte getPage(IPAddress ipBuf,int thisPort, char *page)
+{
+  int inChar;
+  char outBuf[128];
+
+  //Serial.print(F("connecting..."));
+  
+  if(client.connect(ipBuf,thisPort))
+  {
+    //Serial.println(F("connected"));
+
+    sprintf(outBuf,"GET %s HTTP/1.1",page);
+    client.println(outBuf);
+    sprintf(outBuf,"Host: %s",serverName);
+    client.println(outBuf);
+    client.println(F("Connection: close\r\n"));
+  } 
+  else
+  {
+    Serial.println(F("failed"));
+    return 0;
+  }
+  
+  // connectLoop controls the hardware fail timeout
+  int connectLoop = 0;
+
+  while(client.connected())
+  {
+    while(client.available())
+    {
+      inChar = client.read();
+      Serial.write(inChar);
+      // set connectLoop to zero if a packet arrives
+      connectLoop = 0;
+    }
+
+    connectLoop++;
+
+    // if more than 10000 milliseconds since the last packet
+    if(connectLoop > 10000)
+    {
+      // then close the connection from this end.
+      Serial.println();
+      Serial.println(F("Timeout"));
+      client.stop();
+    }
+    // this is a delay for the connectLoop timing
+    delay(1);
+  }
+
+  //Serial.println();
+  //Serial.write(inChar);
+  
+  if(inChar == '1')
+  {
+    Serial.print("switch 1");
+    digitalWrite(9,HIGH);
+    digitalWrite(8,LOW);
+  }
+  else if(inChar == '2')
+  {
+    Serial.print("switch 2");
+    digitalWrite(9,LOW);
+    digitalWrite(8,HIGH);
+  }
+  else 
+  {
+    Serial.write(inChar);
+    digitalWrite(9,LOW);
+    digitalWrite(8,LOW);
+  }
+  Serial.println();
+  Serial.println(F("disconnecting."));
+  // close client end
+  client.stop();
+
+  return 1;
+}
+
